8c517bd1fb814b5a7f9d1e2fb16a79b2324c0ab2
matshel <mats.helgesen@protonmail.com>
2022-12-05 03:01:02 +0100

    coded

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+
+// import logo
+import Logo from "../assets/img/logo.png";
+
+// import components
+import Nav from "./Nav";
+import NavMobile from "./NavMobile";
+
+// import icons
+import { FaBars } from "react-icons/fa";
+import { BsArrowRight } from "react-icons/bs";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [navMobile, setNavMobile] = useState(false);
+  return (
+    <header
+      className='mb-12 lg:mb-0 z-20 relative px-4 lg:px-0'
+      data-aos='fade-down'
+      data-aos-delay='1200'
+      data-aos-duration='1000'
+    >
+      <div className='container mx-auto'>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center gap-x-[120px]'>
+            {/* logo */}
+            <a href='/'>
+              <img src={Logo} alt='' />
+            </a>
+
+            {/* nav / initially hidden  */}
+            <div className='hidden lg:flex'>
+              <Nav />
+            </div>
+          </div>
+
+          {/* mobile nav / initially is shown */}
+          <div
+            className={`${
+              navMobile ? "max-h-52" : "max-h-0"
+            } lg:hidden absolute top-24 bg-accent-tertiary w-full left-0 right-0 font-bold rounded transition-all overflow-hidden`}
+          >
+            <NavMobile />
+          </div>
+
+          <button className='btn btn-quaternary flex items-center gap-x-[20px] group'>
+            Request Demo
+            <BsArrowRight className='text-2xl text-accent-primary group-hover:text-white transition' />
+          </button>
+
+          {/* hamburger menu */}
+          <div
+            onClick={() => setNavMobile(!navMobile)}
+            className='text-2xl text-primary cursor-pointer lg:hidden'
+          >
+            <FaBars />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
